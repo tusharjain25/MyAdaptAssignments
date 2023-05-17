47289cb4822b94fbe6aaa49ab720f0141c4027de
Tushar jain <tusharjain25tj2001@gmail.com>
2023-05-17 14:40:13 +0530

    ques1 added

--- src/adaptAssignment/StringAssignment/question1.java
+++ src/adaptAssignment/StringAssignment/question1.java
@@ -1,6 +1,6 @@
-package adaptAssignment;
+package adaptAssignment.StringAssignment;
 
-public class stringAssignment {
+public class question1 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
---
+++ src/adaptAssignment/basicDataStructureAssignment/Assignment1question1.java
@@ -0,0 +1,43 @@
+package adaptAssignment.basicDataStructureAssignment;
+
+import java.util.Scanner;
+
+public class Assignment1question1 {
+    static class Armstrong {
+        int power(int x, long y) {
+            if (y == 0)
+                return 1;
+            if (y % 2 == 0)
+                return power(x, y / 2) * power(x, y / 2);
+            return x * power(x, y / 2) * power(x, y / 2);
+        }
+
+        int order(int x) {
+            int n = 0;
+            while (x != 0) {
+                n++;
+                x = x / 10;
+            }
+            return n;
+        }
+
+        boolean isArmstrong(int x) {
+
+            int n = order(x);
+            int temp = x, sum = 0;
+            while (temp != 0) {
+                int r = temp % 10;
+                sum = sum + power(r, n);
+                temp = temp / 10;
+            }
+            return (sum == x);
+        }
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            Armstrong ob = new Armstrong();
+            int x = sc.nextInt();
+            System.out.println(ob.isArmstrong(x));
+        }
+    }
+}
